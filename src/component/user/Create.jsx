9e2e876074dc8cf1ebdc0237c92ef2d6c7242c9a
mozammel <mozammel169649@gmail.com>
2024-02-04 23:56:06 +0600

    create user data

--- src/component/user/Create.jsx
+++ src/component/user/Create.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import { Form , Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch } from 'react-redux';
+import { addUser } from './UserSlice';
+
 
 function Create() {
+
+  const dispatch = useDispatch();
+  const [newUser, setNewUser] = useState({ email: "", title: "" });
+  // console.log(newUser)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = newUser;
+    // console.log(user)
+    dispatch(addUser(user));
+  }
+
+
   return (
     <div>
       <h3 className='text-center mt-2'>Create user</h3>
       <div className='col-md-4 mt-2 mx-auto'>
-        <Form >
-          
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>User Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter UserName" />
-            </Form.Group>
+        <Form onSubmit={handleSubmit}>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Title</Form.Label>
-              <Form.Control type="text" placeholder="Enter Title" />
-            </Form.Group>
-            
-            <Button variant="primary" type="submit">
-               Submit
-            </Button>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>User Email</Form.Label>
+            <Form.Control type="text" onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} placeholder="Enter UserName" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" onChange={(e) => setNewUser({ ...newUser, title: e.target.value })} placeholder="Enter Title" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
         </Form>
       </div>
     </div>
