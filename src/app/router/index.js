b250f127ea5850aae0e79cc1937bb6d0e3db17cf
mozammel <mozammel169649@gmail.com>
2024-02-04 18:46:04 +0600

    add nav

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -3,19 +3,17 @@ import Home from '../../component/Home.'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Create from '../../component/user/Create';
 import Update from '../../component/user/Update ';
+import Nav from '../../component/user/nav';
 
 function Index() {
 
     return (
         <BrowserRouter>
+        <Nav/>
           <Routes>
             <Route path="/" element={<Home/>}/> 
-          </Routes>
-          <Routes>
-            <Route path="/create" element={<Create/>}/> 
-          </Routes>
-          <Routes>
-            <Route path="/update" element={<Update/>}/> 
+            <Route path="/create" element={<Create/>}/>          
+            <Route path="/edit" element={<Update/>}/> 
           </Routes>
         </BrowserRouter>
       );
