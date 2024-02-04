f88ac5f5ee5c16e7685d1018e4b62716c45103d0
abs-kawser <abskawser77000@gmail.com>
2024-02-04 16:08:41 +0600

    first commit

---
+++ src/component/user/Update .jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Form , Button } from 'react-bootstrap'
+
+function Update() {
+  return (
+    <div>
+      <h3 className='text-center mt-2'>User Update</h3>
+      <div className='col-md-4 mt-2 mx-auto'>
+        <Form >
+          
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>User Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter UserName" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Enter Title" />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+               Submit
+            </Button>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default Update
