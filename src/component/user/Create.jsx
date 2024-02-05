f36617ef9eb217465b4148b95327beb2012288bc
mozammel <mozammel169649@gmail.com>
2024-02-05 11:15:44 +0600

    set navigate

--- src/component/user/Create.jsx
+++ src/component/user/Create.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
-import { useDispatch } from 'react-redux';
-import { addUser } from './UserSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from './UserSlice.js';
+import { useNavigate } from 'react-router-dom';
 
 
 function Create() {
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [newUser, setNewUser] = useState({ email: "", title: "" });
   // console.log(newUser)
@@ -15,6 +16,7 @@ function Create() {
     const user = newUser;
     // console.log(user)
     dispatch(addUser(user));
+    navigate("/",{replace: true});
   }
 
 
@@ -26,7 +28,7 @@ function Create() {
 
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>User Email</Form.Label>
-            <Form.Control type="text" onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} placeholder="Enter UserName" />
+            <Form.Control type="text" onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} placeholder="Enter email" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -38,6 +40,7 @@ function Create() {
             Submit
           </Button>
         </Form>
+
       </div>
     </div>
   )
